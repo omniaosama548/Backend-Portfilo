6d573663e548c727145cc1b2c658d691d1421fdb
omnia <anaomnia47@gmail.com>
2025-09-08 18:02:05 +0300

    handle-

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import projectRoutes from "./routes/projectRoutes.js";
-import contactRoutes from "./routes/contactRoutes.js"
+import contactRoutes from "./routes/contactRoutes.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -18,16 +18,17 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve uploads folder
+// Serve uploads folder (✅ خليه هنا مرة واحدة بس)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 // Routes
 app.use("/projects", projectRoutes);
-app.use("/contact",contactRoutes)
+app.use("/contact", contactRoutes);
+
 // Swagger setup
 const swaggerOptions = {
   definition: {
@@ -51,4 +52,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
